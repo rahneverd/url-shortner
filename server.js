e70e7191236053a986bc38bd666e5c11e4ebf4ab
rahneverd <rahneverd@gmail.com>
2022-07-27 10:42:53 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/shortUrls", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.post("/shortUrls", (req, res) => {
+    
+})
+
+app.listen(process.env.PORT || 3000)
